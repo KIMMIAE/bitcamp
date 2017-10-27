02dd72ade160433fa90ff886170cd7b27377a3e9
MIAE KIM <kma613@naver.com>
2017-10-27 18:08:26 +0900

    '10.27'

--- java-project/src/main/java/java100/app/App_4.java
+++ java-project/src/main/java/java100/app/App_4.java
@@ -1,11 +1,9 @@
 package java100.app;
+
 import java.util.Scanner;
-
-// Score 객체의 주소를 저장하고 꺼내는 코드를 별도의 클래스로 분리하라!
-
-
-public class App_4 {
-
+ 
+public class App_4 { 
+    
     static boolean confirm(String message) {
         Scanner keyScan = new Scanner(System.in);
         System.out.print(message);
@@ -17,24 +15,25 @@ public class App_4 {
     }
     
     public static void main(String[] args) {
-        // ArrayList는 Score 타입뿐만 아니라 (...)
-        ArrayList scoreDao = new ArrayList();
+        
+        // ArrayList는 Score 타입뿐만 아니라 자바의 모든 타입의 객체에 대해서도 
+        // 사용할 수 있다.
+        ArrayList list = new ArrayList();
         
         while (true) {
-            Score score = new Score();
-            score.input();  // 사용자로부터 입력받은 데이터를 빈 객체에 저장한다.
+            Score score = new Score(); // 성적 데이터를 저장할 빈 객체를 준비한다.
+            score.input(); // 사용자로부터 입력받은 데이터를 빈 객체에 저장한다.
             
-            // 인스턴스( 주소)를 배열에 저장한다.
             list.add(score);
-            
+
             if (!confirm("계속하시겠습니까? "))
                 break;
         }
         
         for (int i = 0; i < list.size(); i++) {
-            //list.get(i).print();
-            
-            ((Score))list.get((i)).print();
-        }
+ 
+            ((Score)list.get(i)).print();
         }
     }
+}
+
