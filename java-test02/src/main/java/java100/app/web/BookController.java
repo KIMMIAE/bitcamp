0c441b467fdc7587e5879029be66467d96d19e4a
miae <kma613@naver.com>
2018-01-10 14:05:41 +0900

    '상세보기'

--- java-test02/src/main/java/java100/app/web/BookController.java
+++ java-test02/src/main/java/java100/app/web/BookController.java
@@ -1,15 +1,13 @@
 package java100.app.web;
 
-import java.io.File;
-import java.io.IOException;
-
-import javax.servlet.ServletContext;
+import java.util.HashMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java100.app.domain.Book;
 import java100.app.service.BookService;
@@ -18,57 +16,51 @@ import java100.app.service.BookService;
 @RequestMapping("/book")
 public class BookController {
     
-    @Autowired ServletContext servletContext;
     @Autowired BookService bookService;
     
-    @RequestMapping(value="add", method=RequestMethod.GET)
-    public String form() {
+    @RequestMapping("list")
+    public String list(
+            @RequestParam(value="words", required=false) String[] words,
+            @RequestParam(value="oc", required=false) String orderColumn,
+            @RequestParam(value="al", required=false) String align,
+            Model model) throws Exception {
+
+        // UI 제어와 관련된 코드는 이렇게 페이지 컨트롤러에 두어야 한다.
+        //
+        
+        HashMap<String,Object> options = new HashMap<>();
+        if (words != null && words[0].length() > 0) {
+            options.put("words", words);
+        }
+        options.put("orderColumn", orderColumn);
+        options.put("align", align);
+        
+        model.addAttribute("list", bookService.list(options));
+        return "book/list";
+    }
+/*
+	@RequestMapping("{no}")
+	public String view(@PathVariable int no, Model model) throws Exception {
+
+		model.addAttribute("book", bookService.get(no));
+		return "book/view";
+	}
+*/
+    
+    
+    
+    
+    @RequestMapping("form")
+    public String form() throws Exception {
         return "book/form";
+        
     }
     
-    @RequestMapping(value="add", method=RequestMethod.POST)
-    public String add(Book book, MultipartFile file) throws Exception {
-        
-        // 업로드 파일을 저장할 폴더 위치를 가져온다.
-        String uploadDir = servletContext.getRealPath("/download");
-        
-        String filename = writeUploadFile(file, uploadDir);
-        book.setPhoto(filename);
+    @RequestMapping("add")
+    public String add(Book book) throws Exception {
         
         bookService.add(book);
-        
-        return "book/form";
+        return "redirect:list";
     }
     
-    long prevMillis = 0;
-    int count = 0;
-    
-    // 다른 클라이언트가 보낸 파일명과 중복되지 않도록 
-    // 서버에 파일을 저장할 때는 새 파일명을 만든다.
-    synchronized private String getNewFilename(String filename) {
-        long currMillis = System.currentTimeMillis();
-        if (prevMillis != currMillis) {
-            count = 0;
-            prevMillis = currMillis;
-        }
-        
-        return  currMillis + "_" + count++ + extractFileExtName(filename); 
-    }
-    
-    // 파일명에서 뒤의 확장자명을 추출한다.
-    private String extractFileExtName(String filename) {
-        int dotPosition = filename.lastIndexOf(".");
-        
-        if (dotPosition == -1)
-            return "";
-        
-        return filename.substring(dotPosition);
-    }
-    
-    private String writeUploadFile(MultipartFile part, String path) throws IOException {
-        
-        String filename = getNewFilename(part.getOriginalFilename());
-        part.transferTo(new File(path + "/" + filename));
-        return filename;
-    }  
 }
