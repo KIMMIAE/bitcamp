1f12c9a2503d28b20f9f1b916ed7b2e30103ef9b
miae <kma613@naver.com>
2018-01-30 20:04:01 +0900

    '.'

---
+++ node-basic/step04/test10.js
@@ -0,0 +1,21 @@
+// 객체 II - 배열과 for문
+
+var arr = [10, 20, 30, 40];
+for (var i in arr) {
+    console.log(arr[i])
+}
+
+console.log("-----------------------------")
+
+for (var value of arr) {
+    console.log(value);
+}
+
+console.log("-----------------------------")
+
+arr.forEach(function(value, index) {
+    console.log(index, value)
+    
+})
+
+    
